892bd937f200120b2fa9823f0bb89b8d885abb1d
zegster <dngo1996@gmail.com>
2020-02-14 19:37:50 -0600

    initial commit

---
+++ RollCall/app/src/main/java/edu/umsl/duc_ngo/rollcall/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.umsl.duc_ngo.rollcall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
